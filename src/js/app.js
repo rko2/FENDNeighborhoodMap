eeb36c3eb6aeb6eb5de3bbc1afaa8214abcb8d44
Ryan <rko2@uchicago.edu>
2016-04-28 22:11:22 -0500

    google maps api

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,24 @@
+var Locations = [{
+  name: "Name1",
+  address: "Address1"
+},
+{
+  name: "Name2",
+  address: "Address2"
+}];
+
+var Location = function(data) {
+  this.name = ko.observable(data.name);
+  this.address = ko.observable(data.address);
+}
+
 var ViewModel = function() {
   var self = this;
+
+  this.placelist = ko.observableArray([]);
+  Locations.forEach(function(locationitem) {
+    self.placelist.push(new Location(locationitem));
+  });
 }
+
+ko.applyBindings(new ViewModel());
