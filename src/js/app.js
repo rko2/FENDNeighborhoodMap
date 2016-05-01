df3a92ddc5d7d83a9b2d2fa55e3b5fa6b69a514a
Ryan <rko2@uchicago.edu>
2016-04-30 23:22:33 -0500

    integrate map into app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,136 @@
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 41.908730,
+      lng: -87.679385
+    },
+    zoom: 15,
+    styles: [{
+      featureType: 'poi',
+      stylers: [{
+          visibility: 'on'
+        }] // Turn on points of interest.
+    }, {
+      featureType: 'transit.station',
+      stylers: [{
+          visibility: 'off'
+        }] // Turn off bus stations, train stations, etc.
+    }],
+    disableDoubleClickZoom: true
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+/*
+// requests for places search API - NOT searchbox. Temporary.
+
+  var storerequest = {
+    location: {
+      lat: 41.908730,
+      lng: -87.679385
+    },
+    radius: 1000,
+    types: ['store']
+  };
+
+  var foodrequest = {
+    location: {
+      lat: 41.908730,
+      lng: -87.679385
+    },
+    radius: 1000,
+    types: ['restaurant']
+  };
+
+  var infowindow = new google.maps.InfoWindow();
+
+  service = new google.maps.places.PlacesService(map);
+
+  //Two requests for different places searches. Temporary.
+  service.nearbySearch(storerequest, callback);
+  service.nearbySearch(foodrequest, callback);
+
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        createMarker(results[i]);
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location,
+      visible: true
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+  */
+}
+
 var Locations = [{
   name: "Name1",
   address: "Address1"
