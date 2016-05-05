827ca4fe66da5c7f8b5db4c5e6bbc4ff4cb3ef69
Ryan <rko2@uchicago.edu>
2016-05-04 21:18:22 -0500

    lay down foundation in JS, html, and CSS

--- src/js/app.js
+++ src/js/app.js
@@ -5,52 +5,22 @@ var service;
 var infowindow;
 
 function initMap() {
+  // Set default center.
+  var WickerPark = new google.maps.LatLng(41.908730, -87.679385);
+
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 41.908730,
-      lng: -87.679385
-    },
+    center: WickerPark,
     zoom: 15,
-    styles: [{
-      featureType: 'poi',
-      stylers: [{
-          visibility: 'on'
-        }] // Turn on points of interest.
-    }, {
-      featureType: 'transit.station',
-      stylers: [{
-          visibility: 'off'
-        }] // Turn off bus stations, train stations, etc.
-    }],
     disableDoubleClickZoom: true
   });
-}
-
-var Locations = [{
-  name: "Name1",
-  address: "Address1"
-}, {
-  name: "Name2",
-  address: "Address2"
-}];
-
-var Location = function(data) {
-  this.name = ko.observable(data.name);
-  this.address = ko.observable(data.address);
 }
 
 var ViewModel = function() {
   var self = this;
 
-  this.placelist = ko.observableArray([]);
-  Locations.forEach(function(locationitem) {
-    self.placelist.push(new Location(locationitem));
-  });
+  this.Term = ko.observable('Tattoo Shops');
 
-  this.currentplace = ko.observable(this.placelist()[0]);
-
-  this.SetPlace = function(clickedPlace) {
-    self.currentplace(clickedPlace);
+  this.updateResults = function(searched) {
   };
 }
 
