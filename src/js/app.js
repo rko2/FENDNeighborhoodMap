c988216f3d55abf5bd317a95a900e8a1f394ad4a
Ryan <rko2@uchicago.edu>
2016-05-07 23:45:09 -0500

    create markers for yelp results

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 var ViewModel = function() {
   var self = this;
 
-  this.Term = ko.observable('Tattoo Shops');
+  this.Term = ko.observable('Tattoo Shop');
 
   this.updateResults = function(searched) {
     ko.computed(function() {
       Yelp('60622', self.Term());
     }, self)
-  };
+  }
 }
 
 ko.applyBindings(new ViewModel());
