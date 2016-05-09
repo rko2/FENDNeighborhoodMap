7da0e4fe9f321befe6c68fd3e29a7c53b9d974c6
Ryan <rko2@uchicago.edu>
2016-05-09 00:16:23 -0500

    cosmetic changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 var ViewModel = function() {
   var self = this;
 
-  this.Term = ko.observable('Tattoo');
+  this.Term = ko.observable('Bars');
 
   this.updateResults = function(searched) {
     ko.computed(function() {
