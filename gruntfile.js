d116d03c9bc347bcbba110790a393b010f213188
Ryan <rko2@uchicago.edu>
2016-05-10 00:05:20 -0500

    remove inline css from gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,28 +1,18 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-inline-css');
 
   grunt.initConfig({
     uglify: {
       dist: {
         files: {
-          'dist/js/app.js' : 'src/js/app.js'
+          'dist/js/app.js' : 'src/js/app.js',
+          'dist/js/map.js' : 'src/js/map.js'
         }
       }
     },
-    inlinecss: {
-        main: {
-            options: {
-            },
-            files: {
-                'dist/index.html': 'src/index.html'
-            }
-        }
-    }
   });
 
   grunt.registerTask('default', [
-    'uglify',
-    'inlinecss'
+    'uglify'
 	]);
 }
